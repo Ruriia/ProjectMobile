d2e88db018fff5fa0ed7eeecfdbfabe4ea01f7fc
Alfon <alfonsusanto@gmail.com>
2019-10-22 17:07:50 +0700

    Betulin beberapa

---
+++ app/src/main/java/finalproject/cargo/SplashLogin.java
@@ -0,0 +1,29 @@
+package finalproject.cargo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_login);
+
+        final int loading_time = 3000;
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+
+                Intent login=new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(login);
+                finish();
+
+            }
+        },loading_time);
+    }
+}
