15c8a8a369acc49ef5356cbe62c614427744a11d
aswincandra <aswincandra@gmail.com>
2019-10-22 12:36:55 +0700

    Pusing

--- app/src/main/java/finalproject/cargo/HomeActivity.java
+++ app/src/main/java/finalproject/cargo/HomeActivity.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.Toolbar;
@@ -22,14 +23,14 @@ public class HomeActivity extends AppCompatActivity {
         getSupportActionBar().setCustomView(R.layout.action_bar); // action_bar.xml
         //getSupportActionBar().setElevation(0);
         View view = getSupportActionBar().getCustomView();
-        TextView name = view.findViewById(R.id.name);
-        name.setOnClickListener(new View.OnClickListener() {
+
+        ImageView btnSearch = view.findViewById(R.id.btnSearch);
+        btnSearch.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                Toast.makeText(HomeActivity.this, "You have clicked tittle", Toast.LENGTH_LONG).show();
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "Clicked", Toast.LENGTH_SHORT).show();
             }
         });
-
     }
 
 }
