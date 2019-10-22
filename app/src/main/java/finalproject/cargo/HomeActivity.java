57798fee3e8db6894b170aed12a56dffeace553f
aswincandra <aswincandra@gmail.com>
2019-10-22 16:34:03 +0700

    Brrrr

--- app/src/main/java/finalproject/cargo/HomeActivity.java
+++ app/src/main/java/finalproject/cargo/HomeActivity.java
@@ -18,19 +18,5 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        this.getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
-        getSupportActionBar().setDisplayShowCustomEnabled(true);
-        getSupportActionBar().setCustomView(R.layout.action_bar); // action_bar.xml
-        //getSupportActionBar().setElevation(0);
-        View view = getSupportActionBar().getCustomView();
-
-        ImageView btnSearch = view.findViewById(R.id.btnSearch);
-        btnSearch.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(getApplicationContext(), "Clicked", Toast.LENGTH_SHORT).show();
-            }
-        });
     }
-
 }
