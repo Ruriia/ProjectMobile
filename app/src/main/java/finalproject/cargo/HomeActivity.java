5a669bc9ae5656c33b6fe428fff1038c8b05e01e
aswincandra <aswincandra@gmail.com>
2019-10-22 10:26:53 +0700

    Start build home page

--- app/src/main/java/finalproject/cargo/HomeActivity.java
+++ app/src/main/java/finalproject/cargo/HomeActivity.java
@@ -1,8 +1,14 @@
 package finalproject.cargo;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.Toolbar;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -10,5 +16,20 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        this.getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        getSupportActionBar().setDisplayShowCustomEnabled(true);
+        getSupportActionBar().setCustomView(R.layout.action_bar); // action_bar.xml
+        //getSupportActionBar().setElevation(0);
+        View view = getSupportActionBar().getCustomView();
+        TextView name = view.findViewById(R.id.name);
+        name.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(HomeActivity.this, "You have clicked tittle", Toast.LENGTH_LONG).show();
+            }
+        });
+
     }
+
 }
