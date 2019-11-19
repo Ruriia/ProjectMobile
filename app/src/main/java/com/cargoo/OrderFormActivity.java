7c5cbec26f3dc471b6bb3f4cb8647ae90234bcf7
Alfon <alfonsusanto@gmail.com>
2019-11-19 12:30:22 +0700

    select image from galery

--- app/src/main/java/com/cargoo/OrderFormActivity.java
+++ app/src/main/java/com/cargoo/OrderFormActivity.java
@@ -1,14 +1,46 @@
 package com.cargoo;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class OrderFormActivity extends AppCompatActivity {
-
+    private static final int image_id = 1;
+    Button upload;
+    TextView imagename;
+    ImageView previewimage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order_form);
+        upload = findViewById(R.id.uploadimage);
+        previewimage = findViewById(R.id.previewimage);
+
+        upload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent galery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(galery, image_id);
+            }
+        });
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == image_id && resultCode == RESULT_OK && data != null){
+            Uri selectedImage = data.getData();
+            previewimage.setImageURI(selectedImage);
+        }
+    }
+
+
 }
