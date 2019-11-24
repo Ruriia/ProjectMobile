bc8698aea97dcfbb5d9115877affe9752ac97aab
Yoga <antoniusyoga54@gmail.com>
2019-11-24 16:26:44 +0700

    activity_order_complete

---
+++ app/src/main/java/com/cargoo/activity_order_complete.java
@@ -0,0 +1,14 @@
+package com.cargoo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_order_complete extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order_complete);
+    }
+}
