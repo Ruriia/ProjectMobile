f9f64b97c24a241adcebfa6827c431b0ab5ff67e
Alfon <alfonsusanto@gmail.com>
2019-11-29 00:35:44 +0700

    sedikit update

--- app/src/main/java/com/cargoo/AccountSetting.java
+++ app/src/main/java/com/cargoo/AccountSetting.java
@@ -1,14 +1,42 @@
 package com.cargoo;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class AccountSetting extends AppCompatActivity {
-
+    private static final int image_id = 1;
+    ImageView previewimage;
+    Uri selectedImage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account_setting);
+        previewimage = findViewById(R.id.profile_image);
+        Button editfoto = findViewById(R.id.btneditfoto);
+        editfoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent galery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(galery, image_id);
+            }
+        });
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == image_id && resultCode == RESULT_OK && data != null) {
+            selectedImage = data.getData();
+            previewimage.setImageURI(selectedImage);
+        }
+    }
+
 }
