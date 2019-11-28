820dfe3541df4ebc1a24ee335fd77d90e307775e
aswincandra <aswincandra@gmail.com>
2019-11-28 16:23:13 +0700

    Checkout + Payment Activity Done

--- app/src/main/java/com/cargoo/CheckoutActivity.java
+++ app/src/main/java/com/cargoo/CheckoutActivity.java
@@ -14,6 +14,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -46,6 +47,8 @@ public class CheckoutActivity extends AppCompatActivity {
 
     private String refDestName, refDestAddress, refDestFullAddress, refPickupName, refPickupAddress, refPickupFullAddress, refPickupDate;
     private String refDeliveryPrices, refTotalPrices;
+
+    private Button btnPayment, btnCancel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -107,7 +110,16 @@ public class CheckoutActivity extends AppCompatActivity {
             }
         });
 
-
+        btnPayment = findViewById(R.id.btnPayment);
+        btnPayment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(CheckoutActivity.this, activity_order_complete.class);
+                i.putExtra("totalPrice", refTotalPrices);
+                startActivity(i);
+                finish();
+            }
+        });
     }
 
     private void retrieveItemData() {
--- app/src/main/java/com/cargoo/activity_order_complete.java
+++ app/src/main/java/com/cargoo/activity_order_complete.java
@@ -2,13 +2,65 @@ package com.cargoo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.squareup.okhttp.internal.Util;
 
 public class activity_order_complete extends AppCompatActivity {
 
+    private TextView txtTotalPrices, txtWA;
+
+    private String refTotalPrice;
+    private ImageView imgCopy;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order_complete);
+
+        Intent i = getIntent();
+        refTotalPrice = i.getStringExtra("totalPrice");
+
+        txtTotalPrices = findViewById(R.id.txtTotalPriceDone);
+        txtTotalPrices.setText(refTotalPrice);
+
+        imgCopy = findViewById(R.id.imageCopy);
+        imgCopy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String norek = "5211070987";
+
+                ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+                ClipData clip = ClipData.newPlainText("norek", norek);
+                clipboard.setPrimaryClip(clip);
+
+                Toast.makeText(activity_order_complete.this, "The bank account number has copied", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        txtWA = findViewById(R.id.textWA);
+        txtWA.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String phone = "081546164842";
+
+                ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+                ClipData clip = ClipData.newPlainText("phone", phone);
+                clipboard.setPrimaryClip(clip);
+
+                Toast.makeText(activity_order_complete.this, "The phone number has copied", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
