b87ac6d8ba03d4717a55146f49832fe1d0647e6d
Alfon <alfonsusanto@gmail.com>
2019-11-30 01:15:37 +0700

    final push dari alfon

--- app/src/main/java/com/cargoo/AccountActivity.java
+++ app/src/main/java/com/cargoo/AccountActivity.java
@@ -7,26 +7,83 @@ import androidx.fragment.app.Fragment;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FileDownloadTask;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import java.io.File;
+import java.io.IOException;
 
 public class AccountActivity extends Fragment {
 
     private FirebaseAuth firebaseAuth;
     private TextView btnlogout;
 
+    private FirebaseAuth fbAuth = FirebaseAuth.getInstance();;
+    private FirebaseUser fbUser = fbAuth.getInstance().getCurrentUser();
+    private String fbUserId = fbUser.getUid();
+    private String fbUserEmail = fbUser.getEmail();
+    private DatabaseReference dbUsers;
+    private StorageReference storageReference;
+
     private TextView txtHistory;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.activity_account, container, false);
+        final TextView company = v.findViewById(R.id.textView18);
+        final ImageView profile = v.findViewById(R.id.profile_image);
+        dbUsers = FirebaseDatabase.getInstance().getReference().child("Users");
+        Query q1 = FirebaseDatabase.getInstance().getReference().child("Users")
+                .orderByChild("userID")
+                .equalTo(fbUserId);
+        q1.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                company.setText(dataSnapshot.child(fbUserId).child("name").getValue(String.class));
+                try {
+                    final File file = File.createTempFile("image","jpg");
+                    storageReference = FirebaseStorage.getInstance().getReferenceFromUrl("gs://cargoo-d9aa1.appspot.com/").child(String.valueOf(dataSnapshot.child(fbUserId).child("profileimage").getValue(String.class)));
+                    storageReference.getFile(file).addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
+                            Bitmap bitmap = BitmapFactory.decodeFile(file.getAbsolutePath());
+                            profile.setImageBitmap(bitmap);
+                        }
+                    });
+                }catch (IOException e){
+                    Toast.makeText(getContext(), e.getMessage(),Toast.LENGTH_LONG).show();
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                
+            }
+        });
         final TextView accountsetting = v.findViewById(R.id.btnAccountSetting);
         accountsetting.setOnClickListener(new View.OnClickListener() {
             @Override
